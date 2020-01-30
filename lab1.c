cc9038f3a227f45205dff970ee3eb62a724d43d4
20171CSE0126 <60209037+20171CSE0126@users.noreply.github.com>
2020-01-30 10:17:49 +0530

    Add files via upload

---
+++ lab1.c
@@ -0,0 +1,17 @@
+<html>
+<head>
+<title>HTML Table Cellpadding<\title>
+<\head>
+<body>
+<table border="1"cellpadding="5"cellspacing="5">
+<tr>
+<th>Name<\th>
+<th>Salary<\th>
+<\tr>
+<tr>
+<tD>Ramesh Raman<\td>
+<td>5000<\td>
+<\tr>
+<\table>
+<\body>
+<\html>
